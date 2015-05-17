9c4ed6211565ac9320144ee96ae687ca8d55ef33
Daniel Garcia <zlapper@gmail.com>
2015-05-17 16:16:54 -0500

    js, css improvements

--- idekratia/static/js/project.js
+++ idekratia/static/js/project.js
@@ -1,6 +1,9 @@
 /* Project specific Javascript goes here. */
 
 
-$(".alert").fadeTo(2000, 500).slideUp(500, function(){
-    $(.alert).alert('close');
-});
+$(".alert")
+	.fadeTo(2000, 500)
+	.slideDown(500, function(){
+    	$(".alert").alert('close');
+	})
+;
