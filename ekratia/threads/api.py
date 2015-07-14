0143c28876dbdf6ba6330cba2eb5c0f86d15ab36
Julian Cantillo <umbrasoft@gmail.com>
2015-07-14 00:00:47 -0500

    app_main isolated api, threads apps created instead, basic templates created

---
+++ ekratia/threads/api.py
@@ -0,0 +1,120 @@
+
+#Imports for django rest framework
+from .serializers import ThreadSerializer
+from .serializers import CommentSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+
+from .models import Thread, Comment
+
+
+class ThreadList(APIView):
+    """
+    API class for list all threads, or create a new thread.
+    """
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get(self, request, format=None):
+        threads = Thread.objects.all()
+        serializer = ThreadSerializer(threads, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        threads = ThreadSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ThreadDetail(APIView):
+    """
+    API class for retrieve, update or delete a thread instance.
+    """
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get_object(self, pk):
+        try:
+            return Thread.objects.get(pk=pk)
+        except Thread.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        threads = self.get_object(pk)
+        serializer = ThreadSerializer(threads)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        thread = self.get_object(pk)
+        serializer = ThreadSerializer(thread, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        thread = self.get_object(pk)
+        thread.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CommentList(APIView):
+    """
+    API class for list all comments from a thread, or create a new comment.
+    """
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get_thread(self, pk):
+        try:
+            return Thread.objects.get(pk=pk)
+        except Thread.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        thread = self.get_thread(pk)
+        comments = Comment.objects.filter(thread=thread)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        comments = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetail(APIView):
+    """
+    API class for retrieve, update or delete a comment instance.
+    """
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get_object(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        comments = self.get_object(pk)
+        serializer = CommentSerializer(comments)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        comment = self.get_object(pk)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- ekratia/threads/urls.py
+++ ekratia/threads/urls.py
@@ -7,18 +7,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from .views import ThreadListView, ThreadCreate
+from .views import ThreadListView, ThreadCreateView, ThreadDetailView
 
 
 urlpatterns = [
-    url(r'^$', ThreadListView.as_view(template_name='app_main/threads.html'), name="threads"),
-    url(r'^create$', ThreadCreate.as_view(template_name='app_main/create_thread.html'), name="create-threads"),
-    url(r'^success$', TemplateView.as_view(template_name='app_main/success_thread.html'), name="success-thread"),
-
-    # url(r'^api/v1/threads/$', ThreadList.as_view()),
-    # url(r'^api/v1/threads/(?P<pk>[0-9]+)/$', ThreadDetail.as_view()),
-    # url(r'^api/v1/comments/thread/(?P<pk>[0-9]+)/$', CommentList.as_view()),
-    # url(r'^api/v1/comments/$', CommentList.as_view()),
-    # url(r'^api/v1/comments/(?P<pk>[0-9]+)/$', CommentDetail.as_view()),
+    url(r'^$', ThreadListView.as_view(), name="threads"),
+    url(r'^detail/(?P<slug>[\w\-\:\.0-9\s]+)/$', ThreadDetailView.as_view(),
+        name='detail'),
+    url(r'^create$', ThreadCreateView.as_view(), name="create-threads"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
