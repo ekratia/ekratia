d580b0c3c7dda191152166908a453ccc131937c1
andresgz <andresgz@gmail.com>
2015-10-02 07:37:14 -0500

    Refactor Threads admin. Replace deprecated functions

--- ekratia/threads/admin.py
+++ ekratia/threads/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
+from ekratia.threads.models import Thread, Comment
 
-# Register your models here.
-
-from django.db.models import get_models, get_app
-
-
-for model in get_models(get_app('threads')):
-    admin.site.register(model)
+# Register Thread models in the admin
+admin.site.register(Thread)
+admin.site.register(Comment)
