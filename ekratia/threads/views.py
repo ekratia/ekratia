ecc2525940e471875a2488322b3f6fe1bd518f07
andresgz <andresgz@gmail.com>
2015-10-05 16:11:09 -0500

    Add user to thread based on current logged in

--- ekratia/threads/views.py
+++ ekratia/threads/views.py
@@ -3,6 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib import messages
 
 from .models import Thread
+from .forms import ThreadForm
 
 
 class ThreadListView(ListView):
@@ -19,9 +20,16 @@ class ThreadCreateView(CreateView):
     """
     model = Thread
     template_name = 'threads/create.html'
-    fields = ['title', 'description', 'user']
+    form_class = ThreadForm
 
     def form_valid(self, form):
+        """
+        If the form is valid, save the associated model.
+        """
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object = form.save(commit=True)
+
         messages.add_message(self.request, messages.SUCCESS,
                              _('New thread created'))
         return super(ThreadCreateView, self).form_valid(form)
