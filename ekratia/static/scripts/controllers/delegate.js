8618ec7f584db1606deee0812d5ae9ff92a75e62
andresgz <andresgz@gmail.com>
2015-10-13 11:17:46 -0500

    Add base view for delegates

---
+++ ekratia/static/scripts/controllers/delegate.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontApp.controller:CommentsController
+ * @description
+ * # CommentsController
+ * Controller of the application
+ * Handles information from the API to be connected with the view
+ */
+angular.module('Ekratia')
+  .controller('DelegatesController',
+    ['$scope',
+    function ($scope) {
+
+    
+}]);
