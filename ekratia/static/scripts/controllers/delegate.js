17abdc2f7e419b2c59588cef5afc6dc66dc772c9
andresgz <andresgz@gmail.com>
2015-10-13 16:20:01 -0500

    Implement Base UI and Angular Controller to list and Add delegates

--- ekratia/static/scripts/controllers/delegate.js
+++ ekratia/static/scripts/controllers/delegate.js
@@ -12,6 +12,26 @@ angular.module('Ekratia')
   .controller('DelegatesController',
     ['$scope',
     function ($scope) {
+        $scope.delegates = [];
+
+        $scope.loadDelegates = function(){
+            $scope.delegates = [
+            {"user": {"username": "Andres Gonzalez", avatar: 'http://placehold.it/75x75/'}},
+            {"user": {"username": "William Perez", avatar: 'http://placehold.it/75x75/'}},
+            {"user": {"username": "William Perez", avatar: 'http://placehold.it/75x75/'}},
+            {"user": {"username": "William Perez", avatar: 'http://placehold.it/75x75/'}},
+            {"user": {"username": "William Perez", avatar: 'http://placehold.it/75x75/'}},
+            ];
+        };
+
+        $scope.addDelegate = function(){
+            $scope.delegates.push({"user": {"username": "New Delegate", avatar: 'http://placehold.it/75x75/'}});
+        };
+
+        $scope.undelegate = function(item){
+            var idx = $scope.delegates.indexOf(item);
+            $scope.delegates.splice(idx,1);
+        };
 
     
 }]);
