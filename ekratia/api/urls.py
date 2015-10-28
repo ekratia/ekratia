dccbf4f99b550ce252012ae0bfd6a738542d6c6a
andresgz <andresgz@gmail.com>
2015-10-28 09:49:12 -0500

    Enable Conversations App Stable: Referendums and front pages

--- ekratia/api/urls.py
+++ ekratia/api/urls.py
@@ -2,14 +2,16 @@
 from __future__ import unicode_literals
 from django.conf.urls import patterns, url
 from ekratia.threads import api as threads
+from ekratia.conversations import api as conversations
 from ekratia.referendums import api as referendums
 from ekratia.delegates import api as delegates
 
 urlpatterns = patterns(
     '',
     # Threads CRUD
-    url(r'^threads/$', threads.ThreadList.as_view()),
-    url(r'^threads/(?P<pk>[0-9]+)/$', threads.ThreadDetail.as_view()),
+    url(r'^conversations/$', conversations.ThreadList.as_view()),
+    url(r'^conversations/(?P<pk>[0-9]+)/$',
+        conversations.ThreadDetail.as_view()),
     # Comments CRUD
     url(r'^comments/$', threads.CommentList.as_view()),
     # Threads comments
--- ekratia/threads/forms.py
+++ ekratia/threads/forms.py
@@ -1,20 +1,5 @@
 from django import forms
-from .models import Thread, Comment
-
-
-class ThreadForm(forms.ModelForm):
-    """
-    Thread Model Form
-    """
-    class Meta:
-        model = Thread
-        exclude = ('user', 'slug',)
-
-    def __init__(self, *args, **kwargs):
-        super(ThreadForm, self).__init__(*args, **kwargs)
-        self.fields['description'].widget = forms.Textarea(
-            attrs={'rows': '2', 'ng-model': 'message', 'msd-elastic': '',
-                   'maxlength': '1000'})
+from .models import Comment
 
 
 class ThreadCommentForm(forms.ModelForm):
