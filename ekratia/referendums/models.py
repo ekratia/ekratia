53cee8b9db5e83fdefddad6997c377fe7124c799
andresgz <andresgz@gmail.com>
2015-11-05 17:05:57 -0500

    EK-52 Display updated vote on delegates page

--- ekratia/referendums/models.py
+++ ekratia/referendums/models.py
@@ -9,24 +9,8 @@ from config.settings import common
 from django.conf import settings
 from ekratia.threads.models import Comment
 
+from .managers import ReferendumVotesManager
 import datetime
-
-
-class ReferendumVotesManager(models.Manager):
-    def open_votes(self, user, positive=True):
-        expiration_time = timezone.now() - datetime.timedelta(
-            hours=settings.REFERENDUM_EXPIRE_HOURS)
-        queryset = self.get_queryset()
-        queryset = queryset.filter(user=user)
-        queryset = queryset.filter(
-            referendum__open_time__isnull=False,
-            referendum__open_time__gt=expiration_time)
-        if positive:
-            queryset = queryset.filter(value__gt=0)
-        else:
-            queryset = queryset.filter(value__lt=0)
-
-        return queryset
 
 
 class Referendum(models.Model):
