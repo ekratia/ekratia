59888517ffade7ce87e7bd9ee088f34713a0999f
andresgz <andresgz@gmail.com>
2015-11-16 17:42:08 -0500

    EK-60 Create archived referendums page

--- ekratia/referendums/managers.py
+++ ekratia/referendums/managers.py
@@ -4,6 +4,45 @@ from django.utils import timezone
 import datetime
 
 
+class ReferendumManager(models.Manager):
+    def get_expiration_time(self):
+        return timezone.now() - datetime.timedelta(
+            hours=settings.REFERENDUM_EXPIRE_HOURS)
+
+    def open(self):
+        """
+        Returns all the open referendums
+        """
+        expiration_time = self.get_expiration_time()
+        queryset = self.get_queryset()
+        queryset = queryset.filter(
+            open_time__isnull=False,
+            open_time__gt=expiration_time)
+
+        return queryset
+
+    def created(self):
+        """
+        Returns all the referendums not open for vote
+        """
+        queryset = self.get_queryset()
+        queryset = queryset.filter(open_time__isnull=True)
+
+        return queryset
+
+    def finished(self):
+        """
+        Returns all the referendums finished
+        """
+        expiration_time = self.get_expiration_time()
+        queryset = self.get_queryset()
+        queryset = queryset.filter(
+            open_time__isnull=False,
+            open_time__lt=expiration_time)
+
+        return queryset
+
+
 class ReferendumVotesManager(models.Manager):
     def open_votes(self, user=None):
         """
