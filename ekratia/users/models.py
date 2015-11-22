eb3971cd93859a7155648c2d0571ddc21b3c110a
andresgz <andresgz@gmail.com>
2015-11-22 12:17:38 -0500

    EK-67 Refactor User methods, remove unused

--- ekratia/users/models.py
+++ ekratia/users/models.py
@@ -11,12 +11,37 @@ from ekratia.core.graphs import get_graph_value, GraphEkratia
 from ekratia.delegates.models import Delegate
 from ekratia.referendums.models import Referendum, ReferendumUserVote
 
+from ekratia.core import utils
+
 import logging
 logger = logging.getLogger('ekratia')
 
 
 class User(AbstractUser):
     rank = models.FloatField(default=0.0)
+
+    @property
+    def get_avatar(self):
+        """
+        Gets avatar from Django Avatar or Facebook
+        """
+        if get_primary_avatar(self):
+            return get_primary_avatar(self).avatar.url
+        elif self.socialaccount_set.all().count() > 0:
+            return utils.change_picture_size(
+                self.socialaccount_set.all()[0].get_avatar_url())
+        else:
+            return 'http://placehold.it/75x75/'
+
+    @property
+    def get_full_name_or_username(self):
+        """
+        Get Full Name or Username
+        """
+        if len(self.get_full_name()) > 0:
+            return self.get_full_name()
+        else:
+            return self.username
 
     def __unicode__(self):
         return self.username
@@ -34,6 +59,10 @@ class User(AbstractUser):
                }
 
     def get_available_delegates(self, name=None):
+        """
+        Get users not delegated by me and exclude myself
+        @param name: Filters available users by name
+        """
         queryset = User.objects.exclude(
                                 id__in=Delegate.objects.filter(
                                     user_id=self.id)
@@ -44,11 +73,13 @@ class User(AbstractUser):
         return queryset
 
     def get_users_delegated_to_me(self):
+        """Returns users that delegated me"""
         return User.objects.filter(
             id__in=Delegate.objects.filter(
                 delegate=self).values_list('user_id'))
 
     def get_users_delegated_by_me(self):
+        """Returns users delegated by me"""
         return User.objects.filter(
             id__in=Delegate.objects.filter(
                 user=self).values_list('delegate_id'))
@@ -90,47 +121,16 @@ class User(AbstractUser):
             return False
         return True
 
-    @property
-    def get_avatar(self):
-        """
-        Gets avatar from Django Avatar or Facebook
-        """
-        if get_primary_avatar(self):
-            return get_primary_avatar(self).avatar.url
-        elif self.socialaccount_set.all().count() > 0:
-            return self.change_picture_size(
-                self.socialaccount_set.all()[0].get_avatar_url())
-        else:
-            return 'http://placehold.it/75x75/'
-
-    @property
-    def get_full_name_or_username(self):
-        """
-        Get Full Name or Username
-        """
-        if len(self.get_full_name()) > 0:
-            return self.get_full_name()
-        else:
-            return self.username
-
     def vote_count_for_referendum(self, referendum):
         """
         Calculates vote value depending on Delegates
         """
-        # My weight vote value
-        count = self.get_pagerank_value_referendum(referendum)
-
-        # # value given by users that delegated to me
-        # for user in self.get_users_delegated_to_me():
-        #     count = count + user.get_pagerank_value_referendum(referendum)
-
-        # # value given by delegates
-        # for user in self.get_users_delegated_by_me():
-        #     count = count + user.get_pagerank_value_referendum(referendum)
-
-        return count
+        return self.get_pagerank_value_referendum(referendum)
 
     def get_vote_referendum(self, referendum):
+        """
+        Return the value vote on a referendum if exists
+        """
         try:
             vote = ReferendumUserVote.objects.get(referendum=referendum,
                                                   user=self)
@@ -138,59 +138,12 @@ class User(AbstractUser):
         except ReferendumUserVote.DoesNotExist:
             return None
 
-    def change_picture_size(self, url, width=70, height=70):
-        """
-        Change the facebook url to use a thumbnail
-        """
-        return url.split('?')[0] + u'?width=%i&height=%i' % (width, height)
-
     def get_pagerank(self):
         return self.rank if self.rank > 0 else self.compute_pagerank()
 
     def compute_pagerank(self):
         self.rank = self.get_pagerank_value()
         self.save()
-
-    def compute_pagerank_tuple(self):
-        """
-        Creates a graph and calculates the pagerank for this node.
-        This will not be efficient in any manner, but should suffice
-        until we need to optimize it with a better data structure.
-        """
-        graph = nx.DiGraph()
-        logger.debug("Calculate pagerank for %s" % self)
-
-        visited, queue = set(), [self.id]
-        while queue:
-            current = queue.pop(0)
-            logger.debug('Current graph: %s' % current)
-            if current not in visited:
-                graph.add_node(current)
-                delegates = Delegate.objects.filter(user__id=current)
-                for delegate in delegates:
-                    graph.add_node(delegate.delegate.id)
-                    graph.add_edge(current, delegate.delegate.id)
-                    queue.append(delegate.delegate.id)
-
-                delegated_to_me = Delegate.objects.filter(delegate__id=current)
-                for delegate in delegated_to_me:
-                    graph.add_node(delegate.user.id)
-                    graph.add_edge(delegate.user.id, current)
-                    queue.append(delegate.user.id)
-
-                visited.add(current)
-
-        pagerank_values = nx.pagerank_numpy(graph)
-        num_visited = len(visited)
-        for user_id, rank in pagerank_values.iteritems():
-            user = User.objects.get(pk=user_id)
-            new_rank = rank * num_visited
-            if user.rank != new_rank:
-                user.rank = new_rank
-                user.save()
-
-        self.rank = pagerank_values[self.id]*num_visited
-        return self.rank, pagerank_values
 
     def update_votes(self):
         # Update Votes on Referendums
